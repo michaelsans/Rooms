7a788b0597441673d9660b5496398104f8a2cf02
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-05-31 15:57:56 -0300

    Finally works, kinda

--- Assets/Mapa/Scripts/ImageColorToObject.cs
+++ Assets/Mapa/Scripts/ImageColorToObject.cs
@@ -9,143 +9,168 @@ public class ImageColorToObject : MonoBehaviour
     public Color[] ObjectColor;
     public AreaTemplate Bioma;
     public Sprite sprite;
-    public GameObject agent;
-    public List<Transform> Portas = new List<Transform>();
-    public List<GameObject> agentsList = new List<GameObject>();
-    public Transform[] obj;
+    public GameObject Sala;
+    public Transform[] SalaParaGO;
     public RoomTemplate[] templates;
-    public Transform[] SummonPoints;
     public Transform[] StartingPoints;
-    public int rand;
-    public Collider2D col;
+    private int rand;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsRoom;
     private void Start()
     {
-        col = gameObject.GetComponent<Collider2D>();
-        //RenderRooms();
-        //Testing pushs
         //TODO: colocar a entrada em uma posição aleatoria, a partir da entrada spawnar as proximas salas de uma forma que elas fiquem próximas a sala que eu
         //desejo fazer um caminho.
-
-        rand = Random.Range(0, templates[2].RoomsToInstatiate.Length);
-        var room = Instantiate(templates[2].RoomsToInstatiate[rand].gameObject);
-        rand = Random.Range(0, StartingPoints.Length);
-        room.transform.position = StartingPoints[rand].position;
-        transform.position = room.transform.position;
-
-        for (int i = 1; i < Bioma.TipoDaSala.Length;i++)
-        {
-            var control = transform.position;
-            if (transform.position == control)
-            {
-                Sensing(col, transform.position);
-            }
-            for (int j = 0; j < templates.Length; j++)
-            {
-                if (Bioma.TipoDaSala[i] == templates[j].RoomID && Bioma.NumberOfConections[i] == templates[j].Conections)
-                {
-                    rand = Random.Range(0, templates[j].RoomsToInstatiate.Length);
-                    room = Instantiate(templates[j].RoomsToInstatiate[rand].gameObject);
-                    room.gameObject.name = templates[j].RoomName;
-                    transform.position = room.transform.position;
-                }
-
-                //if(obj[j].tag == "Porta")
-                //{
-                //    Portas.Add(obj[j].transform);
-                //    var agent0 = Instantiate(agent, obj[j].transform.position, Quaternion.identity, obj[j].parent);
-                //    agent0.transform.localScale = new Vector3(0.03f, 0.03f);
-                //    agentsList.Add(agent0);
-                //}
-            }
-        }
-
-        //for (int i = 0; i < Portas.Count - 1; i++)
-        //{
-        //    var aiDestSetter = agentsList[i].GetComponent<AIDestinationSetter>();
-        //    aiDestSetter.target = Portas[i + 1].transform;
-        //}
+        SpawnMap();
+        StartCoroutine(WaitTime());
     }
 
-    //public void InstatiateObjectByPixelColor(Color color, Vector3 position, GameObject parent)
-    //{
-    //    for (int i = 0; i < objectPrefabs.Length; i++)
-    //    {
-    //        if (ObjectColor[i].gamma == color.gamma)
-    //        {
-    //            var obj = Instantiate(objectPrefabs[i], position, Quaternion.identity);
-    //            obj.transform.parent = parent.transform;
-    //        }
-    //    }
-    //}
-
-    //public void RenderRooms()
-    //{
-    //    for (int a = 0; a < Regulador.Length; a++)
-    //    {
-    //        sprite = Regulador[a].GetComponent<SpriteRenderer>().sprite;
-    //        for (int i = 0; i < sprite.rect.size.x; i++)
-    //        {
-    //            for (int j = 0; j < sprite.rect.size.y; j++)
-    //            {
-    //                Color pixelcolor = sprite.texture.GetPixel(i, j);
-
-    //                InstatiateObjectByPixelColor(pixelcolor, new Vector3(Regulador[a].transform.position.x +i, Regulador[a].transform.position.y+j), Regulador[a]);
-    //            }
-    //        }
-    //    }
-    //}
-
-
-    private Vector3 Sensing(Collider2D collision, Vector3 currentPoint)
+    public bool CheckSite()
     {
-        Debug.Log("tentou");
-        rand = Random.Range(0, 8);
-        if (rand <= 2)
-        {
-            transform.position += new Vector3(50, 0);
-            if (collision.tag == "BuildSite")
-                return transform.position;
-            else
-            {
-                transform.position = currentPoint;
-                return transform.position;
-            }
-
-        }
-        else if(rand > 2 && rand < 4)
-        {
-           transform.position += new Vector3(-50, 0);
-            if (collision.tag == "BuildSite")
-                return transform.position;
-            else
-            {
-                transform.position = currentPoint;
-                return transform.position;
-            }
-        }
-        else if( rand > 4 && rand < 6 )
-        {
-            transform.position += new Vector3(0, 50);
-            if (collision.tag == "BuildSite")
-                return transform.position;
-            else
-            {
-                transform.position = currentPoint;
-                return transform.position;
-            }
-        }
-        else
-        {
-            transform.position += new Vector3(0, -50);
-            if (collision.tag == "BuildSite")
-                return transform.position;
-            else
-            {
-                transform.position = currentPoint;
-                return transform.position;
-            }
-        }
-
+        return Physics2D.OverlapCircle(transform.position, 10f, whatIsGround);//This collider will see if im on a build site
+    }
+    public bool CheckRoom()
+    {
+        return Physics2D.OverlapCircle(transform.position, 10f, whatIsRoom);//This collider will see if im on a room
     }
 
+    public void SpawnMap()
+    {
+        // Começando a ficar certo, agora eu tenho que fazer um loop pelos templates, achar o tipo de sala certa e instanciar a sala
+        //no momento eu estou apenas pegando uma sala de templates mas como eu tenho templates diferentes para o mesmo tipo de sala isso esta
+        //errado
+        rand = Random.Range(0, templates[2].RoomsToInstatiate.Length);//Pick a random room
+        var room = Instantiate(templates[2].RoomsToInstatiate[rand].gameObject);//Instantiate that room
+        rand = Random.Range(0, StartingPoints.Length);//Pick a random starting point
+        room.transform.position = StartingPoints[rand].position;//Make that point the room position;
+        transform.position = room.transform.position;//Go to room position
+        room.name = templates[2].RoomName;//nomeia a sala
+        room.transform.parent = Sala.transform;
+    }
+
+    IEnumerator WaitTime()
+    {
+        int contador = 1;
+        while (contador < 16)
+        {
+            Debug.Log(contador);
+            for (int j = 0; j < templates.Length; j++)
+            {   //Esse Loop checa se estou pegando o tipo de sala certo com o numero de conexões certo
+
+                if (Bioma.TipoDaSala[contador] == templates[j].RoomID && Bioma.NumberOfConections[contador] == templates[j].Conections)
+                {
+                    //Preciso que o j só avance quando gera uma sala, subtrair quando nao gera uma sala leva a problemas
+                    //for loops geram loops infinitos, o que fazer?
+
+                    rand = Random.Range(1, 8);//Choosing a random direction to walk
+
+                    if (rand <= 2)// if rand is 1 or 2 i walk to the right
+                    {
+                        transform.position += new Vector3(50, 0);
+                        if (CheckSite() && !CheckRoom())//Check if im on a build site, if yes instantiate a room
+                        {
+                            GenerateRoom(j);
+                            contador++;
+                            break;
+                        }
+                        else//go back and choose a new direction
+                        {
+                            transform.position += new Vector3(-50, 0);
+                        }
+                    }
+                    if (rand > 2 && rand < 5)//if rand is 3 or 4 walk to the left
+                    {
+                        transform.position += new Vector3(-50, 0);
+                        if (CheckSite() && !CheckRoom())//Check if im on a build site, if yes instantiate a room
+                        {
+                            GenerateRoom(j);
+                            contador++;
+                            break;
+                        }
+                        else//go back and choose a new direction
+                        {
+                            transform.position += new Vector3(+50, 0);
+                        }
+                    }
+                    if (rand > 4 && rand < 7)// if rand is 5 or 6 walk up
+                    {
+                        transform.position += new Vector3(0, 50);
+                        if (CheckSite() && !CheckRoom())//Check if im on a build site, if yes instantiate a room
+                        {
+                            GenerateRoom(j);
+                            contador++;
+                            break;
+                        }
+                        else//go back and choose a new direction
+                        {
+                            transform.position += new Vector3(0, -50);
+                        }
+                    }
+                    else// if rand is 7 or 8 walk down
+                    {
+                        transform.position += new Vector3(0, -50);
+                        if (CheckSite() && !CheckRoom())//Check if im on a build site, if yes instantiate a room
+                        {
+                            GenerateRoom(j);
+                            contador++;
+                            break;
+                        }
+                        else//go back and choose a new direction
+                        {
+                            transform.position += new Vector3(0, +50);
+                        }
+                    }
+                }
+                yield return new WaitForSeconds(.0001f);
+            }
+        }
+        Debug.Log("terminou");
+        RenderRooms();
+    }
+
+    private void GenerateRoom(int n)
+    {
+        rand = Random.Range(0, templates[n].RoomsToInstatiate.Length);//Choose a random room of the right type
+        var room = Instantiate(templates[n].RoomsToInstatiate[rand].gameObject);//Instantiate that room
+        room.transform.position = transform.position;
+        room.name = templates[n].RoomName;
+        room.transform.parent = Sala.transform;
+    }
+
+    public void InstatiateObjectByPixelColor(Color color, Vector3 position, GameObject parent)
+    {
+        for (int i = 0; i < objectPrefabs.Length; i++)
+        {
+            if (ObjectColor[i].gamma == color.gamma)
+            {
+                var obj = Instantiate(objectPrefabs[i], position, Quaternion.identity);
+                obj.transform.parent = parent.transform;
+            }
+        }
+    }
+
+    public void RenderRooms()
+    {
+        Debug.Log("começou");
+        SalaParaGO = Sala.GetComponentsInChildren<Transform>();
+
+        for (int a = 1; a < SalaParaGO.Length; a++)
+        {
+            sprite = SalaParaGO[a].GetComponent<SpriteRenderer>().sprite;
+            for (int i = 0; i < sprite.rect.size.x; i++)
+            {
+                for (int j = 0; j < sprite.rect.size.y; j++)
+                {
+                    Color pixelcolor = sprite.texture.GetPixel(i, j);
+
+                    InstatiateObjectByPixelColor(pixelcolor, new Vector3(SalaParaGO[a].transform.position.x + i, SalaParaGO[a].transform.position.y + j), Sala);
+                    Destroy(SalaParaGO[a].gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 10f);
+    }
 }
